6bbddc37fdfaa6342c35e09cb8ba490b882b528d
dghodse <ghodsednyaneshwar@gmail.com>
2021-07-22 13:32:38 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const nav_icon = document.getElementById('nav_icon');
+const ham = document.getElementById('ham');
 const nav_links = document.getElementById('nav_links');
 
-nav_icon.addEventListener('click',() => {
-    nav_links.classList.toggle('show_links')
+ham.addEventListener('click',() => {
+    nav_links.classList.toggle('show')
+    console.log("sdf");
 });
